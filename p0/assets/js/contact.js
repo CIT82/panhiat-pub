333460d854ab7a7d5c858c2da8c62e3c3b2f22d4
panhia-thao <PThao42@my.scccd.edu>
2024-10-01 20:40:25 -0700

    subscribe button works

--- p0/assets/js/contact.js
+++ p0/assets/js/contact.js
@@ -2,6 +2,7 @@ console.log("connected");
 const FORM = document.getElementById("form");
 const SENT = document.getElementById("sent");
 
+
 FORM.addEventListener("submit", function(e){
     e.preventDefault();
     const name = e.target.name.value;
@@ -14,5 +15,22 @@ FORM.addEventListener("submit", function(e){
     SENT.innerHTML = ''; 
     SENT.appendChild(p)
     FORM.reset()
+
+})
+
+
+const SUBFORM = document.getElementById("subform");
+const MSG = document.getElementById("msg");
+
+SUBFORM.addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const email = e.target.email.value;
+    const p = document.createElement('p');
+    p.textContent=`Thank you for subscribing! You’ll now receive updates straight to your email ${email}`;
+
+    MSG.innerHTML = ''; 
+    MSG.appendChild(p);
+    FORM.reset();
 
 })
