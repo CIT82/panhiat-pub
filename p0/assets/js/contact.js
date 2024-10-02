28c3f3e46b11566499b28f7c2a3bcb0268a1ffc7
panhia-thao <PThao42@my.scccd.edu>
2024-10-01 20:02:47 -0700

    js contact form

---
+++ p0/assets/js/contact.js
@@ -0,0 +1,18 @@
+console.log("connected");
+const FORM = document.getElementById("form");
+const SENT = document.getElementById("sent");
+
+FORM.addEventListener("submit", function(e){
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const subject = e.target.subject.value;
+    const message = e.target.message.value;
+    const p = document.createElement('p');
+    p.textContent=`Hello ${name} Thanks for your message ${message}`
+
+    SENT.innerHTML = ''; 
+    SENT.appendChild(p)
+    FORM.reset()
+
+})
